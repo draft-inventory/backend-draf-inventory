3f6ccb5baa228b5d88bb956f8dc644351791beb6
Rut163 <jesicarut144mail.com>
2024-12-13 20:43:37 -0500

    add debugging

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ from flask import Flask
 from config.config import Config
 from common.db import db, migrate
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    
-    # Init SQLAlchemy & Flask-Migrate
-    db.init_app(app)
-    migrate.init_app(app, db)
 
-    # Basic Route
-    @app.route("/")
-    def home():
-        return "<h1>All works for now...</h1>"
-    
-    return app
+app = Flask(__name__)
+app.config.from_object(Config)
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+# Init SQLAlchemy & Flask-Migrate
+db.init_app(app)
+migrate.init_app(app, db)
+
+# Basic Route
+@app.route("/")
+def home():
+    return "<h1>All works for now...</h1>"
+    
+
+
+#if __name__ == "__main__":
+#    app = create_app()
+#    app.run(debug=True)

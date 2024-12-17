695ade2a4b3e30a5e64b8a0ee94f8bb1c292eceb
Rut163 <jesicarut144mail.com>
2024-12-17 18:40:10 -0500

    update index & init

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
+#Import models
+from inventory.domain.models import *
+
 from app import app
+from common.db import db
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 5000)

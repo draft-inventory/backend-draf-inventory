9a94d1d83b6f8274eec82bc9c1ae5abcd81d1969
Rut163 <jesicarut144mail.com>
2024-12-17 18:40:22 -0500

    update database

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/draft-inventory_database')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:sMLSunOqcDvKKhXKkjkxAjtdmjUmwnds@autorack.proxy.rlwy.net/railway')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'secret_key')

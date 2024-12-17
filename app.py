c4f13297756e85b6a37272c92505b6ea6251c15b
Rut163 <jesicarut144mail.com>
2024-12-17 18:39:31 -0500

    update app routes

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 from flask import Flask
 from config.config import Config
-from common.db import db, migrate
+from common.db import db, ma
 
+# Routes
+from inventory.infrastructure.http.category_routes import category_urls
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # Init SQLAlchemy & Flask-Migrate
 db.init_app(app)
-migrate.init_app(app, db)
+ma.init_app(app)
 
-# Basic Route
-@app.route("/")
-def home():
-    return "<h1>All works for now...</h1>"
-    
-
-
-#if __name__ == "__main__":
-#    app = create_app()
-#    app.run(debug=True)
+# Routes
+app.register_blueprint(category_urls,url_prefix = '/categories')

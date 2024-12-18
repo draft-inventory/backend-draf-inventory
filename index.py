088ad252ed9cbfeddf54cf75154627aee83ea227
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-17 19:33:38 -0500

    fix import path for database module in index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 from inventory.domain.models import *
 
 from app import app
-from common.db import db
+from common.db.db import db
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 5000)

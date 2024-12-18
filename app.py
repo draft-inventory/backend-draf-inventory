ad76ff637c37faec5ee28aeb586b170bb0aef080
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-17 19:32:52 -0500

    add system path to app.py for module import resolution

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import sys
+import os
+
 from flask import Flask
 from config.config import Config
-from common.db import db, ma
+from common.db.db import db, ma
 
 # Routes
 from inventory.infrastructure.http.category_routes import category_urls
@@ -11,6 +14,10 @@ app.config.from_object(Config)
 # Init SQLAlchemy & Flask-Migrate
 db.init_app(app)
 ma.init_app(app)
+
+# Add the path to the current file to the system path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 
 # Routes
 app.register_blueprint(category_urls,url_prefix = '/categories')

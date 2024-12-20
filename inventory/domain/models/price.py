c52898e4db59a53786286e473cc7236493c7b409
Rut163 <jesicarut144mail.com>
2024-12-19 21:03:17 -0500

    update datatipe for Price's cost_price & sale_price

--- inventory/domain/models/price.py
+++ inventory/domain/models/price.py
@@ -5,9 +5,9 @@ class Price(db.Model):
     __tablename__ = 'prices'
 
     id = db.Column(db.Integer, primary_key=True)
-    cost_price = db.Column(db.Integer, nullable=False)
-    sale_price = db.Column(db.Integer, nullable=False)
+    cost_price = db.Column(db.Numeric(precision=10, scale=5), nullable=False)
+    sale_price = db.Column(db.Numeric(precision=10, scale=5), nullable=False) 
 
     # Relación uno a uno con productos
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    product = db.relationship('Product', back_populates='price')
+    #product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    #product = db.relationship('Product', back_populates='price')

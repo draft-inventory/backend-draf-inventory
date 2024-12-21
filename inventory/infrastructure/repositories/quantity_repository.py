c4e80683d6a2d208986b17b812ae1ae3d1d0117d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-21 11:48:22 -0500

    add methods to QuantityRepository for retrieving and updating quantities

--- inventory/infrastructure/repositories/quantity_repository.py
+++ inventory/infrastructure/repositories/quantity_repository.py
@@ -2,11 +2,25 @@ from ...domain.models.quantity import Quantity
 from common.db.db import db
 
 
-class QuantityRepository():
+class QuantityRepository:
     @staticmethod
     def create_quantity(initial_quantity, progress_quantity):
         new_quantity = Quantity(
-            initial_quantity=initial_quantity, progress_quantity=progress_quantity)
+            initial_quantity=initial_quantity,
+            progress_quantity=progress_quantity
+        )
         db.session.add(new_quantity)
         db.session.commit()
         return new_quantity
+
+    @staticmethod
+    def get_quantity_by_id(quantity_id):
+        return Quantity.query.get(quantity_id)
+
+    @staticmethod
+    def update_progress_quantity(quantity_id, new_progress_quantity):
+        quantity = Quantity.query.get(quantity_id)
+        if quantity:
+            quantity.progress_quantity = new_progress_quantity
+            db.session.commit()
+        return quantity

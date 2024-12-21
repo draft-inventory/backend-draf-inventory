790464f2c1c7e8a7e145c6c28a903df3d73ffc93
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-12-21 11:46:54 -0500

    add QuantityHistoryService for creating quantity history records with validation

---
+++ inventory/application/services/quantity_history_service.py
@@ -0,0 +1,16 @@
+from ...infrastructure.repositories.quantity_history_repository import QuantityHistoryRepository
+
+
+class QuantityHistoryService():
+    @staticmethod
+    def create_quantity_history(quantity_id, date, sold_quantity, remaining_quantity):
+        if not quantity_id:
+            raise ValueError("Quantity id can't be empity.")
+        if not date:
+            raise ValueError("Date can't be empity.")
+        if sold_quantity < 0:
+            raise ValueError("Sold quantity can't be negative.")
+        if remaining_quantity < 0:
+            raise ValueError("Remaining quantity can't be negative.")
+
+        return QuantityHistoryRepository.create_quantity_history(quantity_id, date, sold_quantity, remaining_quantity)
